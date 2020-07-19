a27fd3bbd2d0608a653b5e53174810e0ea5cd316
Max1412 <m_altuhov@mail.ru>
2020-07-19 15:03:56 +0300

    Clock digital

---
+++ #src/js/main_script/script.js
@@ -0,0 +1,51 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+// import 'nodelist-foreach-polyfill';
+// require('es6-promise').polyfill();
+// require('formdata-polyfill');
+
+// import svgConvert from '../parts/svgConvert.js';
+// import webp from '../parts/webp.js';
+// import show from '../parts/show.js';
+// import galleryBox from '../parts/baguetteBox.js';
+// import popupInit from '../parts/popup.js';
+// import ibgremove from '../parts/ibg.js';
+// import custom from '../parts/custom.js';
+// import menu from '../parts/menu.js';
+// import forms from '../parts/forms.js';
+// import modal from '../parts/modal.js';
+// import validator from '../parts/validator.js';
+// import scroll from '../parts/scroll.js';
+// import inputJS from '../parts/input.js';
+// import aosinit from '../parts/aosinit.js';
+// import objectFit from '../parts/ofi.js';
+// import countAnimated from '../parts/count.js';
+// import sliderOwl from '../parts/owl-slider.js';
+// import sliderSlick from '../parts/slick-slider.js';
+// import fancyboxScript from '../parts/fancybox-script.js';
+// import '@fancyapps/fancybox';
+// import '../libs/retina.min.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	// svgConvert();
+	// webp();
+	// show();
+	// galleryBox();
+	// popupInit();
+	// ibgremove();
+	// custom();
+	// menu();
+	// forms();
+	// modal();
+	// validator();
+	// scroll();
+	// inputJS();
+	// aosinit();
+	// objectFit();
+	// countAnimated();
+	// sliderOwl();
+	// sliderSlick();
+	// fancyboxScript();
+});
